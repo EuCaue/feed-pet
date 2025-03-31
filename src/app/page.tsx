387cf5db315cde0075015341544b15e250e2d9bf
EuCaue <caueadriano.castro@gmail.com>
2025-03-30 21:32:13 -0300

    fix(landing-page): sending user to the right page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { ArrowRightIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
+  const isLoggedIn = false;
+
   return (
     <div className="min-w-[100vw] min-h-[100vh] flex flex-col items-center justify-center gap-5">
       <h1 className="font-bold text-5xl">Feed pet</h1>
@@ -10,7 +12,7 @@ export default function Home() {
         Never forget when to feed your pet again
       </h2>
       <Button asChild variant={"default"} className="hover-btn">
-        <Link href="/app">
+        <Link href={isLoggedIn ? "/app" : "/auth"}>
           Get started <ArrowRightIcon size={24} />
         </Link>
       </Button>
