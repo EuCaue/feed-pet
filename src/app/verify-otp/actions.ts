2d2ad47d9b445dcd72b2966dcf072f598d1c047f
EuCaue <caueadriano.castro@gmail.com>
2025-04-16 23:57:53 -0300

    feat(verify-otp): redirecting user to `account-settings` when it has no name

--- src/app/verify-otp/actions.ts
+++ src/app/verify-otp/actions.ts
@@ -2,36 +2,56 @@
 import { createClient } from "@/lib/supabase/server";
 import { cookies } from "next/headers";
 
-export const verifyOTP = async ({
-  email,
-  token,
-}: {
+type VerifyOTP = {
   email: string;
   token: string;
-}) => {
+  currentUrl: string;
+};
+
+export const verifyOTP = async ({ email, token, currentUrl }: VerifyOTP) => {
   "use server";
   const cookieStore = await cookies();
   const supabase = createClient(cookieStore);
 
-  const { error, data } = await supabase.auth.verifyOtp({
+  const { error } = await supabase.auth.verifyOtp({
     email,
     token,
     type: "email",
   });
-  
-  console.log(data.session)
 
   if (error) {
     console.error(error);
+    const url = new URL(currentUrl, "http://localhost"); // host dummy só pra usar o URL
+    url.searchParams.set("message", "Could not authenticate user");
+
     return {
       success: false,
       message: "Could not authenticate user",
-      url: "/verify-otp?message=Could not authenticate user",
+      url: url.pathname + "?" + url.searchParams.toString(),
+    };
+  }
+  const {
+    data: { user },
+    error: getUserError,
+  } = await supabase.auth.getUser();
+
+  if (getUserError || !user) {
+    return {
+      success: false,
+      message: "User not found.",
     };
   }
 
+  const { data: profile } = await supabase
+    .from("profiles")
+    .select("name, email")
+    .eq("id", user.id)
+    .single();
+
+  const name = profile?.name;
+
   return {
     success: true,
-    url: "/app",
+    url: name ? "/app" : "/account-settings",
   };
 };
