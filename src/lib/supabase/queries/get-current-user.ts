a9da3956f8e139d6750b27fa90bdb4cb28f9b675
EuCaue <caueadriano.castro@gmail.com>
2025-04-18 00:14:42 -0300

    feat(queries): added `get-current-user` query

---
+++ src/lib/supabase/queries/get-current-user.ts
@@ -0,0 +1,19 @@
+import { cookies } from "next/headers";
+import { createClient } from "@/lib/supabase/server";
+
+export default async function getCurrentUser() {
+  const cookieStore = await cookies();
+  const supabase = createClient(cookieStore);
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return null;
+
+  const { data: profile } = await supabase
+    .from("profiles")
+    .select("name, email")
+    .eq("id", user!.id)
+    .single();
+  return profile;
+}
